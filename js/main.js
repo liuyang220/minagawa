dc2a3eaa7424f89795e989677f075c480019d88b
liuyang220 <liuyangg220@gmail.com>
2015-06-09 00:52:18 +0800

    still struggling on the landing page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 
-	$('#hamburger').on('click', function() {
-		$('#menu').toggle();
+	/*$('#hamburger').on('click', function() {
+		$('#menu').show();
+		$('#container').hide();
+	}); */
+
+	$("#hamburger").click(function() {
+		$("#container").toggle();
+		$("#menu").toggle();
+		$("body").css('background-color','#fff');
 	});

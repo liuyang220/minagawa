17ff97fc47aeaebcfc80b7b825f99061617eeac6
liuyang220 <liuyangg220@gmail.com>
2015-06-11 01:25:01 +0800

    removed border, almost done with project.html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 document.querySelector( "#nav-toggle" )
   .addEventListener( "click", function() {
     this.classList.toggle( "active" );
-	$("#container").toggleClass("toggleClass-whiteBorder toggleClass-blueBorder");
 	$("#menu").toggle();
 	$("body").toggleClass("toggleClass-whiteBackground");
 	$("#logo").toggle();

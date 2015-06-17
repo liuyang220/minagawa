1b46d281f7b11614c07f7f0d478f3ee8208c96d0
liuyang220 <liuyangg220@gmail.com>
2015-06-18 00:30:11 +0800

    debugged the navs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.querySelector( "#nav-toggle" )
+/*document.querySelector( "#nav-toggle" )
   .addEventListener( "click", function() {
     this.classList.toggle( "active" );
 	$("#menu").toggle();
@@ -25,6 +25,43 @@ document.querySelector( "#nav-toggle" )
   	});
 
 	$('.motion').click(function() {
+	  		$('.projects-branding, .projects-print, .projects-web').hide();
+	  	});
+  };
+
+  $(document).ready(select);*/
+
+  document.querySelector( "#nav-toggle" )
+  .addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+	$("#menu").toggle();
+	$("body").toggleClass("toggleClass-whiteBackground");
+	$("#logo").toggle();
+	$("h1,hr,#content,#footer").toggle();
+  });
+
+  var select = function() {
+  	$('.all').click(function() {
+  		$('.projects-branding, .projects-print, .projects-motion, .projects-web').show();
+  	});
+
+  	$('.branding').click(function() {
+  		$('.projects-branding, .projects-print, .projects-motion, .projects-web').show();
+  		$('.projects-web, .projects-print, .projects-motion').hide();
+  	});
+
+  	$('.web').click(function() {
+  		$('.projects-branding, .projects-print, .projects-motion, .projects-web').show();
+  		$('.projects-branding, .projects-print, .projects-motion').hide();
+  	});
+
+  	$('.print').click(function() {
+  		$('.projects-branding, .projects-print, .projects-motion, .projects-web').show();
+  		$('.projects-branding, .projects-web, .projects-motion').hide();
+  	});
+
+	$('.motion').click(function() {
+			$('.projects-branding, .projects-print, .projects-motion, .projects-web').show();
 	  		$('.projects-branding, .projects-print, .projects-web').hide();
 	  	});
   };
